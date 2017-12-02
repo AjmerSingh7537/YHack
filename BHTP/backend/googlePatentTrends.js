4484da150ba34e7416e3753d12ad4a5282677603
roykhoury <roy_khoury12@hotmail.com>
2017-12-02 11:51:16 -0500

    Implemented frontend + fixed bugs in backe end

--- BHTP/backend/googlePatentTrends.js
+++ BHTP/backend/googlePatentTrends.js
@@ -1,14 +1,15 @@
+var request = require('request');
 
 const _ = require('lodash');
 const patenter = "https://patents.google.com/xhr/query?url=q=";
-var getNumberOfPatents=(keyword,startTime,endTime)=>
+var getNumberOfPatents=(keyword,startTime,endTime, callback)=>
 {
   let url = patenter+keyword+"+&before=priority:20171102"+"&after=priority:20171202"
   request(url, function(err,res,body)
   {
-        let resultData= JSON.parse(body).total_num_results;
+        let resultData= JSON.parse(body).results.total_num_results;
 
-        console.log(resultData);
+        console.log(JSON.parse(body).results.total_num_results);
         callback(undefined,resultData);
 
   })
