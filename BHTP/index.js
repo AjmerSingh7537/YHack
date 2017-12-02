149366c3ae705f04df5122ecc3de8d75f6cf6a27
AjmerSingh7537 <a_gadreh@encs.concordia.ca>
2017-12-02 00:43:50 -0500

    using Express

--- BHTP/index.js
+++ BHTP/index.js
@@ -1,1 +1,24 @@
-// add stuff
+const express = require('express');
+const path = require('path');
+const rootPath = require('app-root-dir').get();
+const es6Renderer = require('express-es6-template-engine');
+const app = express();
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: false,
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', es6Renderer);
+app.set('views', path.join(__dirname, 'public/views'));
+app.set('view engine', 'html');
+
+app.get('/', function(req, res) {
+ res.render('home');
+});
+
+app.listen(8080, function() {
+  console.log('the application is running');
+});
